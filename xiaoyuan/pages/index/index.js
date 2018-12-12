c817b876690ebf4adb731d5fd564e82a3094f834
xiaodong <xiaodong.yi@ottv.tv>
2018-12-12 20:54:00 +0800

    [xiaoyuan]

--- xiaoyuan/pages/index/index.js
+++ xiaoyuan/pages/index/index.js
@@ -3,6 +3,7 @@ const api = require('../../config/api.js');
 const user = require('../../services/user.js');
 //获取应用实例
 const app = getApp()
+var that;
 Page({
     data: {
         swiperCurrent: 0,// ++
@@ -17,7 +18,9 @@ Page({
         carouselInfo: [],
         wxlogin: 0, // ++ 是否登陆
         hovercoupons: 1, // ++是否移动到优惠券
-        specialList: []
+        specialList: [],
+		images: [],
+	    uploadedImages: []
     },
     onShareAppMessage: function() {
         return {
@@ -50,6 +53,7 @@ Page({
     },
     onLoad: function(options) {
         this.getIndexData();
+		that = this; var objectId = options.objectId; console.log(objectId);
     },
     onReady: function() {
         // 页面渲染完成
@@ -70,10 +74,65 @@ Page({
     onUnload: function() {
         // 页面关闭
     },
-    jump: function(event) {
-        var jurl = event.currentTarget.dataset.url
-        wx.reLaunch({
-            url: jurl,
-        })
-    },
+
+	chooseImage: function () {
+	    // 选择图片
+	    wx.chooseImage({
+	      count: 3, // 默认9
+	      sizeType: ['compressed'],
+	      sourceType: ['album', 'camera'],
+	      // 可以指定来源是相册还是相机，默认二者都有
+	      success: function (res) {
+	        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+	        var tempFilePaths = res.tempFilePaths;
+	        console.log(tempFilePaths);
+	        that.setData({
+	          images: that.data.images.concat(tempFilePaths)
+	        });
+	      }
+	    })
+	  },
+	  // 图片预览
+	  previewImage: function (e) {
+	    //console.log(this.data.images);
+	    var current = e.target.dataset.src
+	    wx.previewImage({
+	      current: current,
+	      urls: this.data.images
+	    })
+	  },
+	  submit: function () {
+		  console.log(that.data.images);
+	    // 提交图片，事先遍历图集数组
+	    that.data.images.forEach(function (tempFilePath) {
+	      new AV.File('file-name', {
+	        blob: {
+	          uri: tempFilePath,
+	        },
+	      }).save().then(
+	        // file => console.log(file.url())
+	      function (file) {
+	        // 先读取
+	        var uploadedImages = that.data.uploadedImages;
+	        uploadedImages.push(file.url());
+	        // 再写入
+	        that.setData({
+	          uploadedImages: uploadedImages
+	        }); console.log(uploadedImages);
+	      }
+	      ).catch(console.error);
+	    });
+	    wx.showToast({
+	      title: '评价成功', success: function () {
+	        wx.navigateBack();
+	      }
+	    });
+	  },
+	  delete: function (e) {
+	    var index = e.currentTarget.dataset.index; var images = that.data.images;
+	    images.splice(index, 1);
+	    that.setData({
+	      images: images
+	    });
+	  }
 })
