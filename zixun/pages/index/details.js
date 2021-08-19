fa5131ed6b1600cb385d78fadcd96ab2a28fcb4e
xiaodong <xiaodong.yi@ottv.tv>
2021-08-19 19:29:29 +0800

    [1.1.3]

---
+++ zixun/pages/index/details.js
@@ -0,0 +1,28 @@
+
+var url = '';//帖子的ID
+
+Page({
+  data:{
+    item:{},
+    hotcomemnt_hidden:false,
+	url:'',
+    dataList: []
+  },
+  onLoad:function(options){
+	this.setData({
+	  url: options.url,
+	});
+    //页面初始化 options为页面跳转所带来的参数
+  },
+
+  loadSuccess:function(e){
+
+	console.log(e.detail.src);
+
+	wx.navigateTo({
+	  url: '/pages/index/index',
+	})
+
+  }
+
+})
